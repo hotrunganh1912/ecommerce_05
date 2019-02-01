554be737353ac8aeea5d18b861f3de51ec639dcc
hotrunganh1912 <hotrunganhht1912@gmail.com>
2019-02-01 21:13:54 +0700

    remember_login

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,12 +4,17 @@ module ProductsHelper
     if !session[:order_id].nil?
       Order.find(session[:order_id])
     else
-      Order.new
-    end
-  end
+      @current_user = current_user
+      unless @current_user == nil
+        Order.new(users_id: @current_user.id)
+      else
+       Order.new()
+     end
+   end
+ end
 
-  private
-  def load_order_id
-    session.delete(:order_id) if Order.find_by(id: session[:order_id]).nil?
-  end
+ private
+ def load_order_id
+  session.delete(:order_id) if Order.find_by(id: session[:order_id]).nil?
+end
 end
